179ef879cdc99edcf501dcdb5eec225e65482db2
Bohdan <lsdovscky@gmail.com>
2018-06-18 15:38:50 +0300

    add filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(sort_column + ' ' + sort_direction)
+    if params[:ratings].present?
+      @ratings = params[:ratings].keys.to_a
+      @movies = Movie.order(sort_column + ' ' + sort_direction).where(rating: @ratings)
+    else
+      @movies = Movie.order(sort_column + ' ' + sort_direction)
+    end
+    @all_ratings = %w[G PG PG-13 R NC-17]
   end
 
   def new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == sort_column) ? "current" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 end
